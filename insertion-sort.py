0fea79c27d7927a46da6cb6eea9a2e5edd1c8bee
Klaida Azizi <66129477+klaidaazizi@users.noreply.github.com>
2021-10-21 21:54:00 -0400

    Create insertion-sort.py

---
+++ insertion-sort.py
@@ -0,0 +1,12 @@
+def insertionsort(arr):
+    for j in range(1,len(arr)):
+        key = arr[j] 
+        # insert arr[j] into sorted sequence arr[0..j-1]
+        i = j - 1
+        while i >= 0 and arr[i] > key:
+            arr[i+1] = arr[i]
+            i = i - 1
+        arr[i+1] = key
+    return arr
+  
+  
